546fa824dd5436f499c09d0d092ef022c6a4a3f4
Naveena Pachava <naveena9.pv@gmail.com>
2023-01-31 18:15:03 -0600

    palindrome & prime code done

--- problems/prime.js
+++ problems/prime.js
@@ -1,6 +1,16 @@
 const isPrime = (number) => {
+
+    if(number==0||number==1)
     return false
+
+   for(i=2;i<number;i++)
+       if(number%i == 0)
+           return false
+    return true
 }
+
+// for(i=2;i<number;i++)
+// number%i==0? isprime*=false :isprime*=true;
 
 const passed = (val) => val ? "PASSED" : "FAILED"
 
@@ -11,6 +21,7 @@ console.log(`Test case 4: 17: ===> ${passed(isPrime(17))} `)
 console.log(`Test case 5: 0: ===> ${passed(!isPrime(0))} `)
 console.log(`Test case 5: 1: ===> ${passed(!isPrime(1))} `)
 console.log(`Test case 6: 2: ===> ${passed(isPrime(2))} `)
+console.log(`Test case 7: 15: ===> ${passed(!isPrime(15))} `)
 
 
 
